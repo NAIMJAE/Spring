66c6f2a8b4bfe64de9dfd56e328a8746d5e0b396
NAIMJAE <skdlawo5985@gamil.com>
2024-03-20 00:27:53 +0900

    24.03.20

--- sboard/src/main/java/kr/co/sboard/repository/ArticleRepository.java
+++ sboard/src/main/java/kr/co/sboard/repository/ArticleRepository.java
@@ -2,13 +2,20 @@ package kr.co.sboard.repository;
 
 import kr.co.sboard.entity.Article;
 import kr.co.sboard.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ArticleRepository extends JpaRepository<Article, Integer> {
+public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     List<Article> findByCateAndParentOrderByNoDesc(String cate, int parent);
+
+    int countByCateAndParent(String cate, int parent);
+
+    Page<Article> findByCateAndParentOrderByNoDesc(String cate, int parent, Pageable pageable);
 }
--- sboard/src/main/java/kr/co/sboard/service/ArticleService.java
+++ sboard/src/main/java/kr/co/sboard/service/ArticleService.java
@@ -7,6 +7,10 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -21,6 +25,7 @@ public class ArticleService {
     // 별도의 빈 등록 필요
     private final ModelMapper modelMapper;
 
+    // 게시글 등록
     public void insertArticle(ArticleDTO articleDTO){
         // etnity로 변환
         Article article = modelMapper.map(articleDTO, Article.class);
@@ -31,6 +36,7 @@ public class ArticleService {
         return null;
     }
 
+    // x
     public List<ArticleDTO> selectArticles(){
         List<Article> articles = articleRepository.findAll();
         List<ArticleDTO> articleDTOs = new ArrayList<>();
@@ -49,13 +55,25 @@ public class ArticleService {
     }
 
     // 게시판별 게시글 조회
-    public List<ArticleDTO> selectArticleForCate(String cate){
-        List<Article> articles = articleRepository.findByCateAndParentOrderByNoDesc(cate, 0);
+    public List<ArticleDTO> selectArticleForCate(String cate, int currentPg){
+        //List<Article> articles = articleRepository.findByCateAndParentOrderByNoDesc(cate, 0);
+        log.info("startNum : "+currentPg);
+        // 페이지 번호, 페이지 크기, 정렬 방식을 지정하여 Pageable 객체 생성
+        Pageable pageable = PageRequest.of((currentPg-1), 10);
+
+        // Repository 메서드에 Pageable 객체 전달하여 페이징 처리된 결과 가져오기
+        Page<Article> articles = articleRepository.findByCateAndParentOrderByNoDesc(cate, 0, pageable);
+
 
         List<ArticleDTO> articleDTOs = new ArrayList<>();
         for (Article article : articles){
             articleDTOs.add(modelMapper.map(article, ArticleDTO.class));
+            log.info(modelMapper.map(article, ArticleDTO.class).toString());
         }
         return articleDTOs;
+    }
+    // 게시글 개수 조회
+    public int selectArticleCount(String cate){
+        return articleRepository.countByCateAndParent(cate, 0);
     }
 }
